530bda9bbff7805b26cc772aa7e8769e19f648d2
kitte anderson <kitteanderson@gmail.com>
2018-12-23 19:22:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 # Add  code here!
 
 def prime?(integer)
-  (2..integer - 1).all? do |x| integer % x != 0
+  if integer >= 2
+  (2..integer - 1).all? do
+    |x| integer % x != 0
   end
-else
-  return false
-end
+  else
+    return false
+  end
 end
