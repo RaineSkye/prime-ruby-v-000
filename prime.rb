b8e66b181e35ae5aa99f8e588d7a7948768c7827
kitte anderson <kitteanderson@gmail.com>
2018-12-23 19:01:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,5 @@
 # Add  code here!
 
 def prime?(n)
-
-  result = true
-
-  for i in 2..n - 1
-    if n % i == 0
-      result = false
-      break
-    end
-  end
-
-  return result
+  (2..n-1).none?{|i| i % n == 0}
 end
-
-#def prime?(n)
-#  (2..n-1).none?{|num| n % num == 0}
-#end
